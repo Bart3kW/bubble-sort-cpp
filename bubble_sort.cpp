adde2a4829a389196fc7ddf9f7cf6deae76d41ec
Bart3kW <bartekpol83@onet.pl>
2022-05-07 21:45:13 +0200

    two functions , asking for numbers and showing table

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
 //bubble sort
+//easy example
+
 using namespace std;
+
+constexpr int howMuch=5;
+
+int table[howMuch];
+
+void showTable();
+void askForNumbers();
 
 int main()
 {
-    
+    cout<<"***********"<<endl;
+    cout<<"BUBBLE SORT"<<endl;
+
+    askForNumbers();
+
+    showTable();
+
+}
+
+void showTable()
+{
+    for(int i = 0; i < howMuch; i++ )
+        cout<<i<<" ";
+
+    cout<<endl;
+
+    for(int i = 0; i < howMuch; i++ )
+        cout<<table[i]<<" ";
+}
+
+void askForNumbers()
+{
+    for(int i = 0 ;i < howMuch; i++)
+    {
+        cout<<"Number "<<i+1<<" = ";
+        cin>>table[i];
+    }
+
 }
