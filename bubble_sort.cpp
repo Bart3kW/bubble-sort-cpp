1c032f47d2a19e22d4ac49bc8f4d238f3dc4db8b
Bart3kW <bartekpol83@onet.pl>
2022-05-09 12:51:54 +0200

    bubble sort function added

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 //bubble sort
 //easy example
-
 using namespace std;
 
 constexpr int howMuch=5;
-
+//-----------------
+//TABLE DECLARATION
 int table[howMuch];
 
-void showTable();
-void askForNumbers();
+//--------------------
+//functions
+void showTable();//function showing table
+void askForNumbers();//asking for numbers to sort
+void bubbleSortUsingTemporaryVariable(int []);//buble sort function
+//------------------
 
 int main()
 {
@@ -20,8 +24,14 @@ int main()
 
     showTable();
 
-}
+    bubbleSortUsingTemporaryVariable(table);//sort
 
+    cout<<endl<<endl<<"Table after bubble sort:"<<endl;
+
+    showTable();
+}
+//-------------
+//FUNCTION SHOWING HOW TABLE LOOK
 void showTable()
 {
     for(int i = 0; i < howMuch; i++ )
@@ -32,13 +42,34 @@ void showTable()
     for(int i = 0; i < howMuch; i++ )
         cout<<table[i]<<" ";
 }
-
+//------------------
+//ASKING FOR NUMBERS TO SORT
 void askForNumbers()
 {
+    cout<<endl;
     for(int i = 0 ;i < howMuch; i++)
     {
         cout<<"Number "<<i+1<<" = ";
         cin>>table[i];
     }
 
+}
+//------------------------------------------------
+//SORTING FUNCTION
+void bubbleSortUsingTemporaryVariable(int table[])
+{
+    int temp;//temporary variable declaration
+
+    for(int i = 0 ; i < howMuch ; i++)
+    {
+        for(int j = 0; j < howMuch -1 ;j++)
+        {
+            if(table[j] > table[j+1])
+                {
+                    temp = table[j];
+                    table[j] = table[j+1];
+                    table[j+1] = temp;
+                }
+        }
+    }
 }
